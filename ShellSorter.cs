68ac603e33139f835bff84066652a2d2f80ddb5c
aweinber18 <112904760+aweinber18@users.noreply.github.com>
2023-11-06 22:56:43 -0500

    manual add

---
+++ ShellSorter.cs
@@ -0,0 +1,35 @@
+﻿using Sorting_Visualization;
+using System;
+
+public class ShellSorter<T> where T : IComparable<T>
+{
+    private SortVisualizer sortVisualizer;
+
+    public ShellSorter(SortVisualizer sv) {
+        sortVisualizer = sv;
+    }
+
+    public T[] Sort(T[] array, int gap)
+    {
+        if (gap == 0)
+            return array;
+        for (int i = 0; i < array.Length - gap - 1; i++)
+        {
+            if (array[i].CompareTo(array[i + gap]) > 0)
+            {
+                Swap(array, i, i + gap);
+            }
+        }
+        return Sort(array, gap / 2);
+    }
+    private void Swap(T[] array, int i, int j)
+    {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        sortVisualizer.ResetRTB();
+    
+        Thread.Sleep(500);
+    }
+
+}
